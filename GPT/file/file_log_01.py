e65dd46d8b222dc0f8eaba26984564fa754d3d47
tianyuanqi <576647468@qq.com>
2026-08-26 16:18:57 +0800

    file

--- GPT/file/file_log_01.py
+++ GPT/file/file_log_01.py
@@ -1,8 +1,6 @@
 with open("server.log", "r", encoding="utf-8") as file:
-    lines = file.readlines()
-
     error_count = 0
-    for line in lines:
+    for line in file:
         if "ERROR" in line:
             error_count += 1
             print(line.strip())
